bb567b85ed485830bd72ff47a02ba7b7ac23412c
Fabio Bernardi Vaz <128534396+FabioVazzz@users.noreply.github.com>
2025-05-28 20:32:23 -0300

    feat: mudança no padrão decorator

--- KAUA_MATHEUS_SILVA/DECORATOR/store.py
+++ KAUA_MATHEUS_SILVA/DECORATOR/store.py
@@ -1,25 +1,53 @@
-from datetime import datetime
-
-class Frutaria:
-    def __init__(self, nome, produtos=[]):
+class Carro:
+    def __init__(self, nome: str, valor: float):
         self.nome = nome
-        self.produtos = produtos
-    
-    def vender(self, produto):
-        """
-            Verifica se temos o produto em estoque e caso sim realiza a venda, senão, retorna dizendo que não temos!
-        """
-        if(produto in self.produtos):
-            print(f"[{datetime.now()}]")
-            print(f"O produto {self.produtos[self.produtos.index(produto)]} foi vendido!")
-        else:
-            print(f"[{datetime.now()}]")
-            print(f"Não temos esse produto em estoque!")
+        self.valor = valor
 
-    def listarProdutos(self):
-        print(self.produtos)
+class CarroAr:
+    """
+        Carro com Ar-Condicionado.
+    """
+    def __init__(self, nome: str, valor: float):
+        self.nome = nome + " + Ar-Condicionado"
+        self.valor = valor + 2000
 
-frutaria = Frutaria("Frutaria da Rosa", ["Laranja", "Pêra", "Maça"])
-frutaria.vender("Pêra")
+class CarroVidro:
+    """
+        Carro com Vidro Elétrico.
+    """
+    def __init__(self, nome: str, valor: float):
+        self.nome = nome + " + Vidro Elétrico"
+        self.valor = valor + 1500
 
-frutaria.vender("Kiwi")
+class CarroSom:
+    """
+        Carro com Som Automotivo.
+    """
+    def __init__(self, nome: str, valor: float):
+        self.nome = nome + " + Som Automotivo"
+        self.valor = valor + 800
+
+class CarroSomVidro:
+    """
+        Carro com Som Automotivo e Vidro Elétrico.
+    """
+    def __init__(self, nome: str, valor: float):
+        self.nome = nome + " + Som Automotivo + Vidro Elétrico"
+        self.valor = valor + 800 + 1500
+
+def apresentaCarro(nome: str, valor: float):
+    """
+        Faz uma apresentação do carro, logando uma mensagem pronta.
+    """
+    print(f"O {nome} está valendo {valor}")
+
+celta = Carro('Celta', 45000)
+celta_c_Ar = CarroAr('Celta', 45000)
+celta_c_Som = CarroSom('Celta', 45000)
+
+celta_c_Som_Vidro = CarroSomVidro('Celta', 45000)
+
+
+apresentaCarro(celta.nome, celta.valor)
+apresentaCarro(celta_c_Ar.nome, celta_c_Ar.valor)
+apresentaCarro(celta_c_Som_Vidro.nome, celta_c_Som_Vidro.valor)
--- KAUA_MATHEUS_SILVA/DECORATOR/store_decorator.py
+++ KAUA_MATHEUS_SILVA/DECORATOR/store_decorator.py
@@ -1,33 +1,45 @@
-import functools
-from datetime import datetime
-
-def log_venda(func):
-
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        print(f"[{datetime.now()}]")
-        return func(*args, **kwargs)
-    return wrapper
-
-class Frutaria:
-    def __init__(self, nome, produtos=[]):
+class Carro:
+    def __init__(self, nome: str, valor: float):
         self.nome = nome
-        self.produtos = produtos
+        self.valor = valor
 
-    @log_venda    
-    def vender(self, produto):
-        """
-            Verifica se temos o produto em estoque e caso sim realiza a venda, senão, retorna dizendo que não temos!
-        """
-        if(produto in self.produtos):
-            print(f"O produto {self.produtos[self.produtos.index(produto)]} foi vendido!")
-        else:
-            print(f"Não temos esse produto em estoque!")
+class addAr:
+    """
+        Adiciona Ar-Condicionado ao Carro.
+    """
+    def __init__(self, carro: Carro):
+        self.nome = carro.nome + " + Ar-Condicionado"
+        self.valor = carro.valor + 2000
 
-    def listarProdutos(self):
-        print(self.produtos)
+class addVidro:
+    """
+        Adiciona Vidro Elétrico ao Carro.
+    """
+    def __init__(self, carro: Carro):
+        self.nome = carro.nome + " + Vidro Elétrico"
+        self.valor = carro.valor + 1500
 
-frutaria = Frutaria("Frutaria da Rosa", ["Laranja", "Pêra", "Maça"])
-frutaria.vender("Pêra")
+class addSom:
+    """
+        Adiciona Som Automotivo ao Carro.
+    """
+    def __init__(self, carro: Carro):
+        self.nome = carro.nome + " + Som Automotivo"
+        self.valor = carro.valor + 800
 
-frutaria.vender("Kiwi")
+def apresentaCarro(nome: str, valor: float):
+    """
+        Faz uma apresentação do carro, logando uma mensagem pronta.
+    """
+    print(f"O {nome} está valendo {valor}")
+
+celta = Carro('Celta', 45000)
+celta_c_Ar = addAr(celta)
+celta_c_Som = addSom(celta)
+
+celta_c_Som_Vidro = addVidro(celta_c_Som)
+
+
+apresentaCarro(celta.nome, celta.valor)
+apresentaCarro(celta_c_Ar.nome, celta_c_Ar.valor)
+apresentaCarro(celta_c_Som_Vidro.nome, celta_c_Som_Vidro.valor)
